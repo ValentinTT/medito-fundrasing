5d05054960e239b0497906ba1feba64fd8352aa3
ValentinTT <valentintapiatorti@gmail.com>
2024-01-18 13:46:38 -0300

    feat: homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,150 @@
-import Image from 'next/image'
+import Heading from "@/components/article/heading";
+import ListItem from "@/components/article/list-item";
+import Paragraph from "@/components/article/paragraph";
+import SectionHeading from "@/components/article/section-heading";
+import SubHeading from "@/components/article/subheading";
+import Banner from "@/components/banner";
+import Hero from "@/components/hero";
+import MainNavbar from "@/components/main-navbar";
+import { Separator } from "@/components/ui/separator";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import DonationCard from "@/components/donation/donation-card";
+import ProgressDonation from "@/components/donation/progress/progress";
+import DonateButton from "@/components/donation/donate-button";
+import ShareButton from "@/components/donation/share-button";
+import DonorsList from "@/components/donation/donors/donors-list";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main className="flex min-h-screen flex-col items-center pt-20 bg-slate-100">
+      <MainNavbar />
+      <Banner />
+      <section className="sm:container flex">
+        <article className="grow flex flex-col items-center bg-white shadow-md pb-14">
+          <Hero src="/header01.jpg" />
+          <div className="pl-2 sm:pl-4">
+            <Heading className="pb-5">
+              Cultivate Calm: Transformative Meditation for Mental Well-being
+            </Heading>
+            <section className="lg:hidden pr-2 sm:px-4">
+              <ProgressDonation />
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+              <div className="flex">
+                <DonateButton />
+              </div>
+              <div className="flex justify-end">
+                <ShareButton />
+              </div>
+            </section>
+            <div className="sm:px-3 pt-2">
+              <SubHeading>Description:</SubHeading>
+              <Separator className="max-w-3xl" />
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+              <SectionHeading>Introduction:</SectionHeading>
+              <Paragraph>
+                Embark on a journey to inner peace and mental well-being with
+                our fundraising campaign for transformative meditation. Join us
+                in creating a community dedicated to fostering mindfulness and
+                mental resilience.
+              </Paragraph>
+              <SectionHeading>About Transformative Meditation:</SectionHeading>
+              <Paragraph>
+                In a world filled with constant noise and stress, transformative
+                meditation offers a sanctuary for self-discovery and mental
+                renewal. Our mission is to make the benefits of meditation
+                accessible to all, empowering individuals to lead healthier,
+                more balanced lives.
+              </Paragraph>
+              <SectionHeading>Why We Need Your Support:</SectionHeading>
+              <Paragraph>
+                Mental well-being is a universal concern, and many individuals
+                face barriers in accessing meditation resources. With your
+                support, we can break down these barriers and make
+                transformative meditation accessible to diverse communities.
+              </Paragraph>
+              <SectionHeading>How Your Contribution Helps:</SectionHeading>
+              <div>
+                <ListItem>
+                  <Paragraph className="p-0">
+                    <span className="font-semibold">
+                      Guided Meditation Programs:
+                    </span>{" "}
+                    Fund meditation programs led by experienced instructors,
+                    providing accessible and inclusive sessions for individuals
+                    of all backgrounds.
+                  </Paragraph>
+                </ListItem>
+                <ListItem>
+                  <Paragraph className="p-0">
+                    <span className="font-semibold">Community Outreach:</span>{" "}
+                    Expand our outreach initiatives to bring the benefits of
+                    meditation to schools, workplaces, and communities that may
+                    not have easy access to such resources.
+                  </Paragraph>
+                </ListItem>
+                <ListItem>
+                  <Paragraph className="p-0">
+                    <span className="font-semibold">
+                      Technology and Resources:
+                    </span>{" "}
+                    Develop user-friendly meditation apps and provide resources
+                    to support individuals on their mindfulness journey.
+                  </Paragraph>
+                </ListItem>
+              </div>
+              <SectionHeading>Q&A Section:</SectionHeading>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+              <Accordion type="single" collapsible className="w-full max-w-3xl">
+                <AccordionItem value="qa-1">
+                  <AccordionTrigger>
+                    Q: What inspired this fundraising campaign?
+                  </AccordionTrigger>
+                  <AccordionContent>
+                    A: This campaign is inspired by the belief that
+                    transformative meditation is a powerful tool for mental
+                    well-being. We&apos;ve witnessed the positive impact it can
+                    have on individuals and want to make these benefits
+                    accessible to as many people as possible.
+                  </AccordionContent>
+                </AccordionItem>
+                <AccordionItem value="qa-2">
+                  <AccordionTrigger>
+                    Q: How are the funds used?
+                  </AccordionTrigger>
+                  <AccordionContent>
+                    A: Your funds will directly support guided meditation
+                    programs, community outreach efforts, and the development of
+                    resources to make transformative meditation accessible to
+                    diverse populations.
+                  </AccordionContent>
+                </AccordionItem>
+                <AccordionItem value="qa-3">
+                  <AccordionTrigger>
+                    Q: Can I see the impact of my donation?
+                  </AccordionTrigger>
+                  <AccordionContent>
+                    A: Absolutely! We&apos;ll provide regular updates on the
+                    expansion of our programs, the communities reached, and the
+                    personal stories of individuals whose lives have been
+                    positively transformed.
+                  </AccordionContent>
+                </AccordionItem>
+              </Accordion>
+            </div>
+            <section className="sm:px-3 lg:hidden pt-2">
+              <SectionHeading>Our Donors:</SectionHeading>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+              <DonorsList />
+            </section>
+          </div>
+        </article>
+        <DonationCard className="hidden lg:block shadow-md sticky top-24" />
+      </section>
     </main>
-  )
+  );
 }
