a689dcc8eea905b6fce92a5f14c68de55480d3ae
ValentinTT <valentintapiatorti@gmail.com>
2024-01-17 22:46:59 -0300

    feat: donor list

---
+++ components/donation/donors/donors-list.tsx
@@ -0,0 +1,25 @@
+"use client";
+import { Icons } from "@/components/icons";
+import DonorItem from "./donor";
+import { useDonors } from "./hook";
+import { useState } from "react";
+import { cn, shuffle } from "@/lib/utils";
+
+const DonorsList = ({ className }: { className?: string }) => {
+  const [donors] = useDonors();
+  const [icons] = useState(
+    shuffle([Icons.sun, Icons.rainbow, Icons.sparkles, Icons.zap, Icons.laugh])
+  );
+
+  return (
+    <div className={cn("", className)}>
+      <div className="flex flex-wrap">
+        {donors.donors.slice(0, 5).map((donor, idx) => {
+          return <DonorItem donor={donor} Icon={icons[idx]} key={idx} />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default DonorsList;
